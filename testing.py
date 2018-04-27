d23a9b675566e84b1801cb97ba1344198a571da3
Ayoub <ayoub.ghrs@gmail.com>
2018-04-27 03:49:14 +0200

    asus

--- testing.py
+++ testing.py
@@ -8,34 +8,16 @@ Created on Fri Apr 20 15:31:42 2018
 from ale_environment import *
 from agent import *
 import rl_tools
-class Ayoub(object):
-    
-    def __init__(self):
-        
-        self.display()
-        
-    def display(self):
-        print("Ayoub")
-        
-        
-class Ghriss(Ayoub):
-    
-    def __ini__(self):
-        
-        super(Ghriss, self).__init__()
-    
-    def display(self):
-        print("Ghriss")
+
+
 game = "breakout"
 env = ALE(game)
 #env = ALE("seaquest.bin")
-agent = DQN(env.states_dim,env.actions_n,'FC',0.99,1.0)
+agent = DQN(env.states_dim,env.actions_n,'FC',0.99,.05)
 #env.step(0)
 print(env.states_dim)
-print(env.getScreenRGB().shape)
 
 for _ in range(10):
-    rollout = rl_tools.rollouts(env, agent, 10, 500)
+    rollout = rl_tools.rollouts(env, agent, 10, 700)
     agent.reinforce(rollout)
     agent.save("leaneard"+game)
-    agent.set_epsilon(agent.eps/2)
